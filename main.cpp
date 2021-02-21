76c55947758ac070645a44c7cd4b3c74ddf4e9c1
PRIMATEC0\elyeshadj.belgacem <elyeshadj.belgacem@technica-engineering.de>
2021-02-21 19:22:27 +0100

    myfirstcommit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{	
+	int a;
+	int b;
+	int sum;
+
+
+	cout << "Enter a number " << endl;
+	cin >> a;
+
+	cout << "Enter another number " << endl;
+	cin >> b;
+	if (a > b){
+		sum = a + b;
+		cout << "the sum is  " << sum << endl;
+	}
+	else cout << "A not greater than B";
+	cin >> sum;
+	
+
+//	std::cout<<"hello world"<<std::endl;
+//std::cin.get(); 
+	return 0;
+}
